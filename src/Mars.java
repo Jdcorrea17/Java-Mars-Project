80b4f36821ec59d9cca80c0b3df097512b887412
Joshua Correa <jdcorrea17@gmail.com>
2022-08-16 16:19:08 -0500

    project done, got two errors

--- src/Mars.java
+++ src/Mars.java
@@ -1,5 +1,5 @@
 public class Mars {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String colonyName = "wildfire";
         int shipPopulation = 300;
         double shipFood = 4000.00;
@@ -7,21 +7,38 @@ public class Mars {
 
         shipFood = shipFood - (shipPopulation * 0.75);
         shipFood = shipFood - (shipPopulation * 0.75);
-    System.out.println(shipFood);
+        System.out.println(shipFood);
         shipFood = shipFood + (shipFood * .5);
 
         shipPopulation = shipPopulation + 5;
-    System.out.println(shipPopulation);
+        System.out.println(shipPopulation);
 
         String landingLocation = "The Ocean";
-        if(landingLocation == "The Plain"){
+        if (landingLocation == "The Plain") {
             System.out.println("Bbzzz Landing on the Plain");
         } else {
             System.out.println("ERROR!! Flight plan already set. Landing on the plain");
         }
+
+        landing = landing(100);
     }
 
-    landingCheck(){
+    public static void LandingCheck(int Loops) throws InterruptedException {
+        for (int i = 0; i <= Loops; i++) {
+            if (((i / 3) == 0) && (i / 5 == 0)) {
+                System.out.println("Keep Center");
+            } else if ((i / 5) == 0) {
+                System.out.println("Right");
+            } else if ((i / 3) == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
 
+        System.out.println("Landed");
+        return false;
     }
 }
+
